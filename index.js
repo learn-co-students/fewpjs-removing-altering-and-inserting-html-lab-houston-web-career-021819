147260060d2d0ab9e6fd592863aa741966e3aea6
sm00th <ezzybrzy@sm00ths-MacBook-Air.local>
2019-04-01 14:38:19 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+function deleteMain() {
+    let main = document.querySelector('main');
+    main.remove();
+}
+
+deleteMain();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerText = "Jack is the champion"
+document.body.appendChild(newHeader);
