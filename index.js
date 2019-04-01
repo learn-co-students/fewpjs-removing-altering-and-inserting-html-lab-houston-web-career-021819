4f07bf45ab1e1a9bc1edf01f68423ba312c29b67
zepher2211 <zepher2211@gmail.com>
2019-04-01 15:25:32 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+var element = document.createElement('div')
+const newHeader = document.createElement('h1')
+newHeader.id = "victory"
+newHeader.innerText = 'Cory is the champion'
+document.body.querySelector("#main").remove()
