f8915042bf38baed31b6c9ba309193b473e0b7bb
JingChen <jingchenjc2019@gmail.com>
2019-04-01 14:10:09 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+main.remove();
+var newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.innerText = "YOUR-NAME is the champion";
+document.body.append(newHeader);
