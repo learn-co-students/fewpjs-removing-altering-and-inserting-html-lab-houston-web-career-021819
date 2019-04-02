150feb01529a9dcbbd80bc2e8a1c7db4c78a0505
github username <github email address>
2019-04-02 08:49:03 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+
 // Write your code here!
+var newHeader = document.createElement('h1')
+document.body.appendChild(newHeader);
+newHeader.id = "victory";
+newHeader.innerText = "Bobby is the champion";
+var removeMain = document.querySelector("#main");
+removeMain.remove();
